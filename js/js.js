0b9b392425c4438cd020caa51fc137977e5d2037
Ramiro Eduardo Solis Castillo <a301633@uach.mx>
2017-05-11 08:48:37 -0600

    Change of method to get JSON (ajax)

--- js/js.js
+++ js/js.js
@@ -2,46 +2,52 @@ var i = 0;
 var imgs = ["img1", "img2", "img3", "img4"];
 
 
-$(document).ready(function(){
-   
+$(document).ready(function () {
+
     setInterval(changeImg, 20000);
-    
-   if(navigator.geolocation){ navigator.geolocation.getCurrentPosition(function(position){     
-       $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=&units=metric&lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&APPID=c1c00899bea54a22f66a4549f6d3eda8", function(data){
-        $("#loc").html(data.name +", "+data.sys.country);
-        $("#wea").html(data.main.temp+"°");
-        $("#sky").html(data.weather[0].main);
-        $("#imgClima").attr("class", getIcon(data.weather[0].main));
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/weather?q=&units=metric&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=c1c00899bea54a22f66a4549f6d3eda8",
+                jsonp: "callback",
+                success: function (data) {
+                    $("#loc").html(data.name + ", " + data.sys.country);
+                    $("#wea").html(data.main.temp + "°");
+                    $("#sky").html(data.weather[0].main);
+                    $("#imgClima").attr("class", getIcon(data.weather[0].main));
+                }
             });
-        }); 
-    }                         
+    });
+}
 });
 
-function changeImg(){
-    
-    
+function changeImg() {
+
+
     $("#backImg").animate({
         opacity: 0
-    }, 1000, function(){
-        $("#backImg").attr("src", "img/"+imgs[i]+".jpg");
+    }, 1000, function () {
+        $("#backImg").attr("src", "img/" + imgs[i] + ".jpg");
     });
-    
+
     $("#backImg").animate({
         opacity: 1
     }, 1000)
-    
-    
-    if(i == 3){
+
+
+    if (i == 3) {
         i = 0;
-    }else{
+    } else {
         i++;
     }
 }
 
-function getIcon(clime){
+function getIcon(clime) {
     clime = clime.toLowerCase();
     console.log(clime);
-    switch(clime){
+    switch (clime) {
         case 'clear':
             return "wi wi-day-sunny";
             break;
@@ -60,30 +66,30 @@ function getIcon(clime){
         case 'thunderstom':
             return "wi wi-day-storm-showers";
             break;
-            
+
     }
 }
 
-function changeTemp(opc){
-    
+function changeTemp(opc) {
+
     var temp = document.getElementById("wea").innerHTML;
-    
+
     var arr = temp.split("");
     arr.pop();
     temp = arr.join("");
-    
-    switch(opc){
-            
+
+    switch (opc) {
+
         case 'C':
             temp = Math.round(parseInt(temp) * 1.8 + 32);
-            document.getElementById("wea").innerHTML = temp+"°";
+            document.getElementById("wea").innerHTML = temp + "°";
             document.getElementById("grados").innerHTML = 'F';
             break;
         case 'F':
             temp = Math.round((parseInt(temp) - 32) / 1.8);
-            document.getElementById("wea").innerHTML = temp+"°";
+            document.getElementById("wea").innerHTML = temp + "°";
             document.getElementById("grados").innerHTML = 'C';
             break;
-      }
-    
+    }
+
 }
